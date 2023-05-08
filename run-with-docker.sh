2ab98b0c50685f7c2ddba941ba64d6ed33a2b94b
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2023-05-08 18:27:14 +0200

    moves to standard folders.

--- run-with-docker.sh
+++ run-with-docker.sh
@@ -1,8 +1,8 @@
 docker run --rm \
 	--user root \
         --name notebook-java-${PWD##*/} \
-        --volume $PWD:/home/jovyan/notebooks \
         --volume $HOME/JUPYTER_WORK:/home/jovyan/work \
+        --volume $PWD:/home/jovyan/work/local \
         --publish 8888:8888 \
         --env NB_UID=$UID \
-        brunoe/jupyter-java-base:develop start-notebook.sh --notebook-dir=/home/jovyan/notebooks
+        brunoe/jupyter-java:develop
